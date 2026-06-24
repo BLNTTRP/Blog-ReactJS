f37cbd84e0deec2ea9c2143fc59a2365e8f1dd8d
Joaquin Cosentino <joacocosentino2003@outlook.com.ar>
2026-06-24 00:51:55 -0300

    design: React logo added to the homepage

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -3,6 +3,16 @@ const Inicio = () => {
         <div style={{ textAlign: 'center', padding: '50px' }}>
             <h1>Bienvenido a mi Blog React</h1>
             <p>Este es el inicio de mi Single Page Application.</p>
+
+            {/* Agregamos el logo de React aquí */}
+            <img
+                src="/React-icon.svg"
+                alt="Logo de React"
+                style={{
+                    width: '150px',
+                    marginTop: '30px'
+                }}
+            />
         </div>
     );
 };
